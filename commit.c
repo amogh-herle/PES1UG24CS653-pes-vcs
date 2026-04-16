7ca7151e09ec4b32053e4f0b1f7927f2f9988c22
amogh-herle <amoghherle07@gmail.com>
2026-04-16 12:48:13 +0530

    Phase 4: serialize commit struct and write to object store

--- commit.c
+++ commit.c
@@ -204,11 +204,18 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     else
         c.has_parent = 0;
 
-    // Step 3: Fill metadata
     snprintf(c.author, sizeof(c.author), "%s", pes_author());
     c.timestamp = (uint64_t)time(NULL);
     snprintf(c.message, sizeof(c.message), "%s", message);
 
-    (void)commit_id_out;
+    // Step 4: Serialize commit struct to text and write to object store
+    void *data;
+    size_t len;
+    if (commit_serialize(&c, &data, &len) != 0) return -1;
+
+    int rc = object_write(OBJ_COMMIT, data, len, commit_id_out);
+    free(data);
+    if (rc != 0) return -1;
+
     return -1;
 }
